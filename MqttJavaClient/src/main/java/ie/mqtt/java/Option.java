84894c5ad6f4285ef5672b2edaac5f8f32a8a38b
Kevin <kevind992@hotmail.com>
2020-04-14 17:25:29 +0100

    Client now communicating with Broker
    Client is now sending commands to the Mqtt broker on the raspberry pi.
    When the client is running the user has the option of turning the light
    on, off or shutting down the connection.
    Currently these commands are being sent as strings (i.e On Command or
    Off Command). This will be changed to deliver protobuf payloads.
    
    This was implemented using the following patterns
    
    - Command pattern
    - Singleton pattern

---
+++ MqttJavaClient/src/main/java/ie/mqtt/java/Option.java
@@ -0,0 +1,8 @@
+package ie.mqtt.java;
+
+public enum Option {
+	ON, 
+	OFF,
+	SHUTDOWN,
+	DEFAULT
+}
--- MqttJavaClient/src/main/java/ie/mqtt/java/Runner.java
+++ MqttJavaClient/src/main/java/ie/mqtt/java/Runner.java
@@ -1,17 +1,59 @@
 package ie.mqtt.java;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
+import java.util.Scanner;
 
 public class Runner {
 	public static void main(String[] args) {
-		Client client = new Client("Hello Shane");
 		
-		try {
-			client.Connect();
-		} catch (MqttException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
+		// HEADER
+		System.out.println("===========================================");
+		System.out.println("MQTT CLIENT");
+		System.out.println("===========================================");
+		System.out.println("Select from the following options");
+		System.out.println("- 1 : Turn light ON \n- 2 : Turn Light OFF \n- 0 : QUIT");
+		System.out.println("===========================================");
+		
+		Scanner scan = new Scanner(System.in);
+		int i_Option = scan.nextInt();
+		
+		Client client = Client.getInstance();
+		Controller controller = new Controller(new LightOnCommand(client), 
+				new LightOffCommand(client), 
+				new ShutdownCommand(client));
+		
+		do {
+			
+			switch (i_Option) {
+			case 1: // Light on
+				System.out.println("Turning Light On!");
+				controller.turnOn();
+				break;
+			case 2: // Light off
+				System.out.println("Turning Light Off!");
+				controller.turnOff();
+				break;
+			case 0: // Shutdown
+				System.out.println("Shutting Down!");
+				controller.shutdown();
+				break;
+			default:
+				System.out.println("Invalid Option!");
+				break;
+			}
+			
+			// HEADER
+			System.out.println("===========================================");
+			System.out.println("Select from the following options");
+			System.out.println("- 1 : Turn light ON \n- 2 : Turn Light OFF \n- 0 : QUIT");
+			System.out.println("===========================================");
+			i_Option = scan.nextInt();
+			
+		} while (i_Option != 0);
+		
+		System.out.println("Good-bye!");
+		
+		scan.close();
 	}
 }
+
